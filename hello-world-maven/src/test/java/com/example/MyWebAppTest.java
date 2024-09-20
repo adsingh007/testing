97e63cfa7ab9aee7354d3f3af63817715fd80741
Amandeep Singh <40333645+adsingh007@users.noreply.github.com>
2024-09-20 10:48:48 +0530

    Create MyWebAppTest.java

---
+++ hello-world-maven/src/test/java/com/example/MyWebAppTest.java
@@ -0,0 +1,14 @@
+// src/test/java/com/example/MyWebAppTest.java
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyWebAppTest {
+
+    @Test
+    public void sampleTest() {
+        assertTrue(true, "Sample test that always passes");
+    }
+}
